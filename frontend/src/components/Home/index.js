ba0590c57adf65b74bcd5bf58542ef8d05f03b4a
shanghlxsg <shanghlxsg@gmail.com>
2022-12-03 13:51:10 +0800

    account data page with api

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,12 +1,61 @@
-import * as React from "react";
-import {NavBar, Logout} from "../NavBar/index"
+import React, { useEffect, useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+const SERVER_PREFIX =
+  "http://ec2-13-215-211-254.ap-southeast-1.compute.amazonaws.com";
+
+const getData = (id) => {
+  return fetch(`${SERVER_PREFIX}/user/bankaccounts`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ userid: id }),
+  });
+};
 
 const Home = () => {
+  const [data, setData] = useState([]);
+  const userid = 1;
+
+  useEffect(() => {
+    getData(userid)
+      .then((res) => res.json())
+      .then((body) => {
+        setData(body.data);
+      });
+  }, []);
+
   return (
-    <div>
-      <NavBar/>
-      <h1>Home</h1>
-    </div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Account&nbsp;ID</TableCell>
+            <TableCell align="right">Account Type</TableCell>
+            <TableCell align="right">AccountBalance</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row) => (
+            <TableRow
+              key={row.AccountID}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.AccountID}
+              </TableCell>
+              <TableCell align="right">{row.AccountType}</TableCell>
+              <TableCell align="right">{row.AccountBalance}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
