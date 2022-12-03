4a1bdb1512ca1585bfa516fb3930d0da9a7534a2
KenGondor <kengondorleh@gmail.com>
2022-12-03 14:06:02 +0800

    add bank account get func

--- frontend/src/api/bankAccountInfoGet.js
+++ frontend/src/api/bankAccountInfoGet.js
@@ -8,9 +8,16 @@ const getBankAccountInfo = async (id) => {
         id
     }
     try {
-
+        let res = await axios.post(ep, payload);
+        let data = res.data;
+        if (data.code === 200) {
+            return data.data;
+        } else {
+            return false;
+        }
     } catch (err) {
-        
+        console.log(err)
+        return false;
     }
 };
 
