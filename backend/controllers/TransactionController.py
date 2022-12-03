9172d2380473a05a114d5460b035fa6c29810606
Sherman Sng <sherman.sng.2019@sis.smu.edu.sg>
2022-12-03 16:45:03 +0800

    Added JWT token on insert & delete transactions

--- backend/controllers/TransactionController.py
+++ backend/controllers/TransactionController.py
@@ -63,79 +63,133 @@ class TransactionController():
 
     def insert_transaction(request): 
         data = request.get_json()
-        try:
-            if len(data) > 0:
-                userID = data["userID"]
-                accountID =  data["accountID"] 
-                receivingaccountID = data['receivingaccountID'] 
-                date =  data['date'] 
-                transactionamount = data['transactionamount'] 
-                comment = data['comment'] 
 
-                #query and match 
+        auth_token = AuthUtil.getAuthToken(request)
+        existingBlacklistToken = AuthUtil.checkBlacklistToken(auth_token)
+        if auth_token != None:
+            if existingBlacklistToken == None:
+                resp = AuthUtil.decode_auth_token(auth_token)
+                if not isinstance(resp, str):
+                    try:
+                        if len(data) > 0:
+                            userID = data["userID"]
+                            accountID =  data["accountID"] 
+                            receivingaccountID = data['receivingaccountID'] 
+                            date =  data['date'] 
+                            transactionamount = data['transactionamount'] 
+                            comment = data['comment'] 
+
+                            #query and match 
 
 
-                #adding instance
-                scheduledtransaction = ScheduledTransaction(AccountID = accountID, ReceivingAccountID = receivingaccountID,
-                                                            Date = date, TransactionAmount=transactionamount, Comment=comment)
+                            #adding instance
+                            scheduledtransaction = ScheduledTransaction(AccountID = accountID, ReceivingAccountID = receivingaccountID,
+                                                                        Date = date, TransactionAmount=transactionamount, Comment=comment)
 
-                #add transaction to db
-                db.session.add(scheduledtransaction)     
-                db.session.commit()                        
-                return jsonify({
-                    "code": 200,
-                    "data": {
-                        "transactionID":scheduledtransaction.TransactionID,
-                        "accountID ":  scheduledtransaction.AccountID,
-                        "receivingaccountID": scheduledtransaction.ReceivingAccountID,
-                        "date":scheduledtransaction.Date,
-                        "transactionamount":scheduledtransaction.TransactionAmount,
-                        "comment":scheduledtransaction.Comment
+                            #add transaction to db
+                            db.session.add(scheduledtransaction)     
+                            db.session.commit()                        
+                            return jsonify({
+                                "code": 200,
+                                "data": {
+                                    "transactionID":scheduledtransaction.TransactionID,
+                                    "accountID ":  scheduledtransaction.AccountID,
+                                    "receivingaccountID": scheduledtransaction.ReceivingAccountID,
+                                    "date":scheduledtransaction.Date,
+                                    "transactionamount":scheduledtransaction.TransactionAmount,
+                                    "comment":scheduledtransaction.Comment
+                                }
+                            })
+                    except Exception as error: 
+                        print(error)
+                        return jsonify(
+                            {
+                                "code": 400,
+                                "data": "Data format error"
+                            }
+                        ),400
+                else:
+                    responseObject = {
+                        'status': 'fail',
+                        'message': resp
                     }
-                })
-        except Exception as error: 
-            print(error)
-            return jsonify(
-                {
-                    "code": 400,
-                    "data": "Data format error"
-                }
-            ),400
+                    return (jsonify(responseObject)), 401
+            else:
+                responseObject = {
+                        'status': 'fail',
+                        'message': 'Invalid Token'
+                    }
+                return (jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                "code": 401,
+                'message': 'Provide a valid auth token'
+            }
+            return (jsonify(responseObject)), 401
+
+        
 
     def delete_Transaction(request): 
         data = request.get_json()
-        try:
-            numOfDeletedTransactions = 0
 
-            if len(data) > 0:
-                for i in data["transactionID"]:
-                    transaction = ScheduledTransaction.query.filter_by(TransactionID=i).first()
-                    if transaction != None:
-                        db.session.delete(transaction)
-                        db.session.commit()
-                        numOfDeletedTransactions += 1
-                        
-                if(numOfDeletedTransactions > 0):
-                    return jsonify({
-                        "code": 200,
-                        "data": {
-                            "message": "Deleted Successfully"
-                        }
-                    })
-                    
+        auth_token = AuthUtil.getAuthToken(request)
+        existingBlacklistToken = AuthUtil.checkBlacklistToken(auth_token)
+        if auth_token != None:
+            if existingBlacklistToken == None:
+                resp = AuthUtil.decode_auth_token(auth_token)
+                if not isinstance(resp, str):
+                    try:
+                        numOfDeletedTransactions = 0
+
+                        if len(data) > 0:
+                            for i in data["transactionID"]:
+                                transaction = ScheduledTransaction.query.filter_by(TransactionID=i).first()
+                                if transaction != None:
+                                    db.session.delete(transaction)
+                                    db.session.commit()
+                                    numOfDeletedTransactions += 1
+                                    
+                            if(numOfDeletedTransactions > 0):
+                                return jsonify({
+                                    "code": 200,
+                                    "data": {
+                                        "message": "Deleted Successfully"
+                                    }
+                                })
+                                
+                            else:
+                                return jsonify({
+                                    "code": 200,
+                                    "data": {
+                                        "message": "No transactions deleted"
+                                    }
+                                })
+
+                    except Exception as error: 
+                        print(error)
+                        return jsonify(
+                            {
+                                "code": 400,
+                                "data": "Data format error"
+                            }
+                        ),400
                 else:
-                    return jsonify({
-                        "code": 200,
-                        "data": {
-                            "message": "No transactions deleted"
-                        }
-                    })
+                    responseObject = {
+                        'status': 'fail',
+                        'message': resp
+                    }
+                    return (jsonify(responseObject)), 401
+            else:
+                responseObject = {
+                        'status': 'fail',
+                        'message': 'Invalid Token'
+                    }
+                return (jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                "code": 401,
+                'message': 'Provide a valid auth token'
+            }
+            return (jsonify(responseObject)), 401
 
-        except Exception as error: 
-            print(error)
-            return jsonify(
-                {
-                    "code": 400,
-                    "data": "Data format error"
-                }
-            ),400
+        
