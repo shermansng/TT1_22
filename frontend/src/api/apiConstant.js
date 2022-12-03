4d2e9c9362b179f732cd781b468b8ec652706c38
KenGondor <kengondorleh@gmail.com>
2022-12-03 14:05:52 +0800

    Add header func

--- frontend/src/api/apiConstant.js
+++ frontend/src/api/apiConstant.js
@@ -1,1 +1,7 @@
 export const URL = 'http://ec2-13-215-211-254.ap-southeast-1.compute.amazonaws.com';
+
+export const getHeader = (token) => {
+    return {
+        "Authorization": `Bearer ${token}`
+    }
+}
