c9f87f7d60f364cb6d23b0af8ec1f6094e70ef00
shanghlxsg <shanghlxsg@gmail.com>
2022-12-03 16:20:34 +0800

    update Home component

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -6,56 +6,70 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { NavBar } from "components/NavBar";
+import getBankAccountInfo from "api/bankAccountInfoGet";
 
-const SERVER_PREFIX =
-  "http://ec2-13-215-211-254.ap-southeast-1.compute.amazonaws.com";
+// const SERVER_PREFIX =
+//   "http://ec2-13-215-211-254.ap-southeast-1.compute.amazonaws.com";
 
-const getData = (id) => {
-  return fetch(`${SERVER_PREFIX}/user/bankaccounts`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ userid: id }),
-  });
-};
+// const getData = (id) => {
+//   return fetch(`${SERVER_PREFIX}/user/bankaccounts`, {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ userid: id }),
+//   });
+// };
 
 const Home = () => {
   const [data, setData] = useState([]);
-  const userid = 1;
+  const [accNo, setAccNo] = useState([]);
+  const userid = parseInt(sessionStorage.getItem("id"));
+
+  const getInfo = async () => {
+    let info = await getBankAccountInfo(userid);
+    setData(info);
+  };
 
   useEffect(() => {
-    getData(userid)
-      .then((res) => res.json())
-      .then((body) => {
-        setData(body.data);
-      });
+    getInfo();
+    // setData();
+    // getData(userid)
+    //   .then((res) => res.json())
+    //   .then((body) => {
+    //     setData(body.data);
+    //   });
   }, []);
 
+  console.log(data);
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Account&nbsp;ID</TableCell>
-            <TableCell align="right">Account Type</TableCell>
-            <TableCell align="right">AccountBalance</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row) => (
-            <TableRow
-              key={row.AccountID}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.AccountID}
-              </TableCell>
-              <TableCell align="right">{row.AccountType}</TableCell>
-              <TableCell align="right">{row.AccountBalance}</TableCell>
+    <div>
+      <NavBar></NavBar>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Account ID</TableCell>
+              <TableCell align="right">Account Type</TableCell>
+              <TableCell align="right">Account Balance</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {data.map((row) => (
+              <TableRow
+                key={row.AccountID}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.AccountID}
+                </TableCell>
+                <TableCell align="right">{row.AccountType}</TableCell>
+                <TableCell align="right">{row.AccountBalance}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 };
 
