998f542d963ebfd14cbc686b20f4b1020d2f678c
Sherman Sng <sherman.sng.2019@sis.smu.edu.sg>
2022-12-03 13:15:17 +0800

    Added logout function to blacklist JWT token

--- backend/main.py
+++ backend/main.py
@@ -36,5 +36,9 @@ def retrieveUserDetails():
 def updateUserDetails():
     return UserController.updateUserDetails(request)
 
+@app.route("/user/logout", methods=["POST"])
+def logoutUser():
+    return UserController.logoutUser(request)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5003, debug=True)
