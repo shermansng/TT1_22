71f262e9143c093f7f7ca8c56d95a1646d9a6e6d
CHINNING <shumchinning@gmail.com>
2022-12-03 13:14:46 +0800

    edit: add response code and error handling for transaction endpoint

--- backend/controllers/TransactionController.py
+++ backend/controllers/TransactionController.py
@@ -25,12 +25,22 @@ class TransactionController():
         data = request.get_json()
         try:
             transactionList = ScheduledTransaction.query.filter(ScheduledTransaction.AccountID == data["accountID"]).all()
-            result = [e.json() for e in transactionList]
-            return jsonify(
-                    {
-                        "data": result
-                    }
-                ),200; 
+            if len(transactionList):
+                result = [e.json() for e in transactionList]
+                return jsonify(
+                        {
+                            "code": 200,
+                            "data": result
+                        }
+                    ),200; 
+            else:
+                return jsonify(
+                {
+                    "code": 400,
+                    "data": "no transactions found"
+                }
+            )
+
         except Exception as error:
             print(error)
             return jsonify(
