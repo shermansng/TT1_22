ea9708a2ae5b8f84ebff71fb3f7887c5948648b3
CHINNING <shumchinning@gmail.com>
2022-12-03 12:50:29 +0800

    add: endpoint for getTransactionbyaccountID

--- backend/controllers/TransactionController.py
+++ backend/controllers/TransactionController.py
@@ -4,22 +4,43 @@ from models.ScheduledTransaction import ScheduledTransaction
 class TransactionController():
     def getTransaction(request):
 
-        if request.method == "GET":
-                
-            try:
-                transactionList = ScheduledTransaction.query.all()
-                result = [e.json() for e in transactionList]
-                return jsonify(
+        try:
+            transactionList = ScheduledTransaction.query.all()
+            result = [e.json() for e in transactionList]
+            return jsonify(
+                {
+                    "data": result
+                }
+            ),200; 
+        except Exception as error:
+            print(error)
+            return jsonify(
+                {
+                    "code": 500,
+                    "data": "Data format error"
+                }
+            )
+
+    def getTransactionsByAccount(request):
+        data = request.get_json()
+        try:
+            transactionList = ScheduledTransaction.query.filter(ScheduledTransaction.AccountID == data["accountID"]).all()
+            result = [e.json() for e in transactionList]
+            return jsonify(
                     {
                         "data": result
                     }
                 ),200; 
-            except Exception as error:
-                print(error)
-                return jsonify(
-                    {
-                        "code": 500,
-                        "data": "Data format error"
-                    }
-                )
+        except Exception as error:
+            print(error)
+            return jsonify(
+                {
+                    "code": 500,
+                    "data": "Data format error"
+                }
+            )
+
+        
+        
+
 
