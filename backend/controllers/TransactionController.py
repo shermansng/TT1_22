62456f9099f8580034ca96ef268798ce5e0931e0
“wanhui98” <yuwanhui.0498@gmail.com>
2022-12-03 14:15:53 +0800

    insert and delete transactions

--- backend/controllers/TransactionController.py
+++ backend/controllers/TransactionController.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for
+from config import app, db
 from models.ScheduledTransaction import ScheduledTransaction
 
 class TransactionController():
@@ -23,3 +24,66 @@ class TransactionController():
                     }
                 )
 
+    def insert_transaction(request): 
+        data = request.get_json()
+        try:
+            if len(data) > 0:
+                # transactionID =  #leave as nan value to be auto populated 
+                userID = data["userID"]
+                accountID =  data["accountID"] 
+                receivingaccountID = data['receivingaccountID'] 
+                date =  data['date'] 
+                transactionamount = data['transactionamount'] 
+                comment = data['comment'] 
+
+                scheduledtransaction = ScheduledTransaction(TransactionID = transactionID , AccountID = accountID, ReceivingAccountID = receivingaccountID,
+                                                            Date = date, TransactionAmount=transactionamount, Comment=comment)
+                #add transaction to db
+                app.db.session.add(scheduledtransaction)     
+                app.db.commit()                        
+            return jsonify({
+                        "code": 200,
+                        "data": {
+                            "transactionID":scheduledtransaction.TransactionID,
+                            "accountID ":  scheduledtransaction.accountID,
+                            "receivingaccountID": scheduledtransaction.receivingaccountID,
+                            "date":scheduledtransaction.date,
+                            "transactionamount":scheduledtransaction.transactionamount,
+                            "comment":scheduledtransaction.comment
+                        }
+                    })
+        except Exception as error: 
+            print(error)
+            return jsonify(
+                {
+                    "code": 400,
+                    "data": "Data format error"
+                }
+            ),400
+
+    def delete(request): 
+        data = request.get_json()
+        try:
+            if len(data) > 0:
+                transaction = ScheduledTransaction.query.filter_by(data[transaction_id])
+                if transaction == None:
+                                return jsonify({
+                                    "code": 404,
+                                    "data": {
+                                        "status": "fail",
+                                        "message": "Transaction not found"
+                                    }
+                                }),404
+                else: 
+                    app.db.session.delete(transaction)
+                    app.db.session.commit()
+                data 
+            return redirect(url_for('getTransaction'))
+        except Exception as error: 
+            print(error)
+            return jsonify(
+                {
+                    "code": 400,
+                    "data": "Data format error"
+                }
+            ),400
